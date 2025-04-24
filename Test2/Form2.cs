91097e86ba4e8d076e3eff58da744265b5f0ca95
Edgar Felipe Hernandez Garcia <loquendero2010@live.com.mx>
2025-04-24 13:48:22 -0600

    Intenta implementar esto

--- Test2/Form2.cs
+++ Test2/Form2.cs
@@ -19,6 +19,7 @@ namespace Test2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Button clicked in Form2");
             Form3 form3 = new Form3();
             form3.ShowDialog();
         }
